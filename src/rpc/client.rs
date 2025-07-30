6857d004cb6abc28ca740804cdd29106f258a041
Shubham Mishra <grapheo12@gmail.com>
2025-07-30 16:32:30 -0700

    Cleaning logs

--- src/rpc/client.rs
+++ src/rpc/client.rs
@@ -531,7 +531,9 @@ impl PinnedClient {
         client: &PinnedClient,
         name: &String,
     ) -> Result<PinnedMessage, Error> {
+        let __get_sock_start = Instant::now();
         let sock = Self::get_sock(client, name, client.0.full_duplex).await?;
+        info!("Get sock latency: {} us", __get_sock_start.elapsed().as_micros());
         let mut lsock = sock.0.lock().await;
         let mut resp_buf = vec![0u8; 256];
         let sz = lsock.get_next_frame(&mut resp_buf).await? as usize;
