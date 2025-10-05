5f7aae59a45c00cee82ed2e5431af9f2de2f3938
Shubham Mishra <grapheo12@gmail.com>
2025-10-04 20:07:41 -0700

    Sequencer sharding

--- src/utils/cache.rs
+++ src/utils/cache.rs
@@ -4,22 +4,24 @@ use lru::LruCache;
 use serde::Serialize;
 
 use crate::{config::RocksDBConfig, utils::types::{CacheKey, CachedValue}};
-use rocksdb::{DBCompactionStyle, Options, WriteBatchWithTransaction, WriteOptions, DB};
+// use rocksdb::{DBCompactionStyle, Options, WriteBatchWithTransaction, WriteOptions, DB};
+use fjall::{Keyspace, PartitionCreateOptions, PartitionHandle};
 
 
 pub struct Cache {
     read_cache: LruCache<CacheKey, CachedValue>,
     db_path: String,
-    db: DB,
+    db: PartitionHandle,
+    keyspace: Keyspace,
 }
 
 impl Cache {
     pub fn new(cache_size: usize, config: RocksDBConfig) -> Self {
-        let mut opts = Options::default();
-        opts.create_if_missing(true);
-        opts.set_write_buffer_size(config.write_buffer_size);
-        opts.set_max_write_buffer_number(config.max_write_buffer_number);
-        opts.set_min_write_buffer_number_to_merge(config.max_write_buffers_to_merge);
+        // let mut opts = Options::default();
+        // opts.create_if_missing(true);
+        // opts.set_write_buffer_size(config.write_buffer_size);
+        // opts.set_max_write_buffer_number(config.max_write_buffer_number);
+        // opts.set_min_write_buffer_number_to_merge(config.max_write_buffers_to_merge);
         // opts.set_target_file_size_base(config.write_buffer_size as u64);
 
         // opts.set_manual_wal_flush(true);
@@ -29,10 +31,18 @@ impl Cache {
 
         // opts.increase_parallelism(3);
 
-        let path = config.db_path.clone();
-        let db = DB::open(&opts, path).unwrap();
+        // let path = config.db_path.clone();
+        let fjall_config = fjall::Config::new(config.db_path.clone())
+            .cache_size(config.write_buffer_size as u64)
+            .compaction_workers(1)
+            .flush_workers(1);
 
-        Self { read_cache: LruCache::new(std::num::NonZero::new(cache_size).unwrap()), db, db_path: config.db_path }
+        let keyspace = fjall_config.open().unwrap();
+        let db = keyspace.open_partition("default", PartitionCreateOptions::default()).unwrap();
+
+        // let db = DB::open(&opts, path).unwrap();
+
+        Self { read_cache: LruCache::new(std::num::NonZero::new(cache_size).unwrap()), db_path: config.db_path, keyspace, db }
     }
 
     pub fn get(&mut self, key: &CacheKey) -> (Option<CachedValue>, bool /* read from cache */) {
@@ -53,11 +63,11 @@ impl Cache {
     }
 
     pub fn put(&mut self, key: CacheKey, value: CachedValue) {
-        let mut wopts = WriteOptions::default();
-        wopts.disable_wal(false);
+        // let mut wopts = WriteOptions::default();
+        // wopts.disable_wal(false);
 
         let ser = bincode::serialize(&value).unwrap();
-        self.db.put_opt(key.clone(), ser, &wopts).unwrap();
+        self.db.insert(key.clone(), ser).unwrap();
         self.read_cache.put(key, value);
     }
 
@@ -71,11 +81,13 @@ impl Cache {
             return true;
         }
 
-        if !self.db.key_may_exist(key) {
-            return false;
-        }
+        // if !self.db.contains_key(key).unwrap() {
+        //     return false;
+        // }
 
-        self.db.get_pinned(key).unwrap().is_some()
+        self.db.contains_key(key).unwrap()
+
+        // self.db.get_pinned(key).unwrap().is_some()
     }
 
     pub fn stats(&self) -> Vec<String> {
@@ -92,10 +104,10 @@ impl Cache {
             }
         });
 
-        let rocksdb_stats = self.db.property_value("rocksdb.stats").unwrap_or_default().unwrap_or_default();
-        let rocksdb_stat_lines = rocksdb_stats.split('\n').collect::<Vec<&str>>();
-        let _n = rocksdb_stat_lines.len();
-        let rocksdb_stats = rocksdb_stat_lines[_n-10..].join("\n");
+        // let rocksdb_stats = self.db.property_value("rocksdb.stats").unwrap_or_default().unwrap_or_default();
+        // let rocksdb_stat_lines = rocksdb_stats.split('\n').collect::<Vec<&str>>();
+        // let _n = rocksdb_stat_lines.len();
+        // let rocksdb_stats = rocksdb_stat_lines[_n-10..].join("\n");
         vec![
             format!("Read Cache size: {}, Max seq num: {} with Key: {}, Second max seq num: {} with Key: {}",
                 self.read_cache.len(),
@@ -103,17 +115,17 @@ impl Cache {
                 max2_seq_num, String::from_utf8(max2_key.clone()).unwrap_or(hex::encode(max2_key))
             ),
 
-            format!("RocksDB stats: {}", rocksdb_stats),
+            // format!("RocksDB stats: {}", rocksdb_stats),
         ]
     }
 }
 
 impl Drop for Cache {
     fn drop(&mut self) {
-        let _ = self.db.flush();
-        let opts = Options::default();
+        // let _ = self.db.flush();
+        // let opts = Options::default();
 
-        let _ = DB::destroy(&opts, &self.db_path);
+        // let _ = DB::destroy(&opts, &self.db_path);
     }
 }
 
